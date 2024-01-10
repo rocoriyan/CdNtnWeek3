bad0369b0dcd347f7b3fc64be23a37cae93ed67c
rocoriyan <rcorrigan.other+github@gmail.com>
2024-01-10 15:27:37 +0000

    added skeleton script code & sounds, some html chnages too

---
+++ script.js
@@ -0,0 +1,18 @@
+let soundBoom = new Audio('sounds/boom');
+let soundClap = new Audio('sounds/clap');
+let soundHihat = new Audio('sounds/hihat');
+let soundKick = new Audio('sounds/kick');
+let soundOpenhat = new Audio('sounds/openhat');
+let soundRide = new Audio('sounds/ride');
+let soundSnare = new Audio('sounds/snare');
+let soundTink = new Audio('sounds/tink');
+let soundTom = new Audio('sounds/tom');
+
+let drumPart = document.querySelectorAll(".drumItem");
+let drumset = document.getElementById('drumset');
+
+drumset.addEventListener('click', (playSound) => {
+    /**/
+});
+
+console.log(drumPart);
