001eea73be6a93f01dbe2bdfa3478456c939b4d0
rocoriyan <rcorrigan.other+github@gmail.com>
2024-01-11 17:59:58 +0000

    styling updates + keypresses work for drum + num at top left of drum tiles

--- drumscript.js
+++ drumscript.js
@@ -1,55 +1,32 @@
-let soundBoom = new Audio('sounds/boom.wav');
-let soundClap = new Audio('sounds/clap.wav');
-let soundHihat = new Audio('sounds/hihat.wav');
-let soundKick = new Audio('sounds/kick.wav');
-let soundOpenhat = new Audio('sounds/openhat.wav');
-let soundRide = new Audio('sounds/ride.wav');
-let soundSnare = new Audio('sounds/snare.wav');
-let soundTink = new Audio('sounds/tink.wav');
-let soundTom = new Audio('sounds/tom.wav');
+const drumItems = [
+    'boom',
+    'clap',
+    'hihat',
+    'kick',
+    'openhat',
+    'ride',
+    'snare',
+    'tink',
+    'tom'
+];
 
-let boom = document.getElementById('boom');
-let clap = document.getElementById('clap');
-let hihat = document.getElementById('hihat');
-let kick = document.getElementById('kick');
-let openhat = document.getElementById('openhat');
-let ride = document.getElementById('ride');
-let snare = document.getElementById('snare');
-let tink = document.getElementById('tink');
-let tom = document.getElementById('tom');
+const drumItemElements = drumItems.map(item => document.getElementById(item));
+const soundElements = drumItems.map(sound => new Audio(`sounds/${sound}.wav `));
 
-boom.addEventListener('click', (playBoom) => {
-    soundBoom.play()
-});
+for(let itemIndex = 0; itemIndex < 9; itemIndex++){
+    drumItemElements[itemIndex].addEventListener('click', function () {
+        playSound(itemIndex);
+    });
+};
 
-clap.addEventListener('click', (playClap) => {
-    soundClap.play()
-});
+function playSound(drumPart){
+    let audio = soundElements[drumPart];
+    audio.play();
+};
 
-hihat.addEventListener('click', (playHihat) => {
-    soundHihat.play()
-});
-
-kick.addEventListener('click', (playKick) => {
-    soundKick.play()
-});
-
-openhat.addEventListener('click', (playOpenhat) => {
-    soundOpenhat.play()
-});
-
-ride.addEventListener('click', (playRide) => {
-    soundRide.play()
-});
-
-snare.addEventListener('click', (playSnare) => {
-    soundSnare.play()
-});
-
-tink.addEventListener('click', (playTink) => {
-    soundTink.play()
-});
-
-tom.addEventListener('click', (playTom) => {
-    soundTom.play()
+document.addEventListener('keypress', (keyClick) => {
+    if(keyClick.code.includes("Digit") && keyClick.code !== "Digit0"){
+        let soundIndex = parseInt(keyClick.key) - 1;
+        playSound(soundIndex);
+    };
 });
