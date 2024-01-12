748e249b3a7facd095665716c7125ad2caa787db
rocoriyan <rcorrigan.other+github@gmail.com>
2024-01-12 10:03:12 +0000

    added comments to script.js and got rid of onclick events when clicking on page u are already on in nav

--- script.js
+++ script.js
@@ -13,21 +13,22 @@ hideButton.addEventListener('click', (event) => { //when you click the button...
 const changeButton = document.getElementById("changeMe");
 
 changeButton.addEventListener('click', (changeImg) => {
-    document.getElementById("changeCat").src = document.getElementById("changeTxt").value;
+    document.getElementById("changeCat").src = document.getElementById("changeTxt").value; //change img source to text found in text input
+    document.getElementById("changeCat").alt = "your chosen image"; //change alt because otherwise it'll say "a cat hiding under the covers" for every image
 });
 // END task 2
 
 // START task 3
 const colourButton = document.getElementById("colourMe");
 
-colourButton.addEventListener('click', (changeColour) => {
-    document.getElementById("task3header").style.color=document.getElementById("favColour").value;
+colourButton.addEventListener('click', (changeColour) => { //when button is clicked...
+    document.getElementById("task3header").style.color=document.getElementById("favColour").value; //recolour the header for task 3
 });
 // END task 3
 
 // START task 4
-document.addEventListener('click', (outputPos) => {
-    document.getElementById("task4p").textContent = `Click co-ordinates - X: ${outputPos.clientX}  Y: ${outputPos.clientY}`;
+document.addEventListener('click', (outputPos) => { //when page is clicked
+    document.getElementById("task4p").textContent = `Click co-ordinates - X: ${outputPos.clientX}  Y: ${outputPos.clientY}`; //output x and y position
 })
 // END task 4
 
